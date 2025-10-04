9ba995bd8fb02a2ffcb9bf0b575de247ec569d3a
kaaato <kaaato@gmail.com>
2025-10-04 10:44:22 +0200

    update

--- frontend/src/services/notes.js
+++ frontend/src/services/notes.js
@@ -1,8 +1,20 @@
 import axios from 'axios'
+/* const baseUrl = 'http://localhost:3001/notes' */ // the original url before part 3.b
+// const baseUrl = 'http://localhost:3001/api/notes'
 const baseUrl = '/api/notes'
 
 const getAll = () => {
   return axios.get(baseUrl).then(response => response.data)
+
+  // error practicing purpose
+/*   const request = axios.get(baseUrl)
+  const nonExisting = {
+    id: 10000,
+    content: 'This note is not saved to server',
+    important: true,
+  }
+  return request.then(response => response.data.concat(nonExisting)) */
+
 }
 
 const create = newObject => {
@@ -14,3 +26,9 @@ const update = (id, newObject) => {
 }
 
 export default { getAll, create, update}
+
+// export default { 
+//   getAll: getAll, 
+//   create: create, 
+//   update: update 
+// }
